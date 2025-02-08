dbb5a60e281e0ef980ee15a4fb837a53a15c2fc5
Eneb <135270219+ametrahs@users.noreply.github.com>
2025-02-08 15:47:59 -0800

    header and footer

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css"
 import React from 'react'
+import Home from "./Pages/Home/Home"
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <div className="App">
+        <Home />
+      </div>
+    </>
+  );
 }
 
 export default App
